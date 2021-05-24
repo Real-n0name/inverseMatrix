ecfcb4d82eda0c0e7e4d0db214707a4eebc0c35c
Egor <karachevegor26@gmail.com>
2021-05-24 17:53:53 +0300

    end

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.label7 = new System.Windows.Forms.Label();
+            this.labelDet = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDown1.Location = new System.Drawing.Point(395, 39);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(76, 30);
+            this.numericUpDown1.TabIndex = 39;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(33, 110);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.Size = new System.Drawing.Size(597, 274);
+            this.dataGridView1.TabIndex = 37;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft PhagsPa", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(648, 151);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 62);
+            this.button1.TabIndex = 36;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Checked = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.radioButton2.Location = new System.Drawing.Point(493, 75);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(121, 28);
+            this.radioButton2.TabIndex = 35;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "случайно";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.radioButton1.Location = new System.Drawing.Point(493, 41);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(113, 28);
+            this.radioButton1.TabIndex = 34;
+            this.radioButton1.Text = "вручную";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(489, 10);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 24);
+            this.label2.TabIndex = 33;
+            this.label2.Text = "Ввод массива";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(29, 43);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(333, 24);
+            this.label3.TabIndex = 31;
+            this.label3.Text = "Размер массива по горизонтали";
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.AllowUserToAddRows = false;
+            this.dataGridView3.AllowUserToDeleteRows = false;
+            this.dataGridView3.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView3.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(861, 110);
+            this.dataGridView3.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersWidth = 62;
+            this.dataGridView3.Size = new System.Drawing.Size(597, 274);
+            this.dataGridView3.TabIndex = 49;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label7.Location = new System.Drawing.Point(855, 35);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(259, 31);
+            this.label7.TabIndex = 50;
+            this.label7.Text = "Обратная матрица:";
+            // 
+            // labelDet
+            // 
+            this.labelDet.AutoSize = true;
+            this.labelDet.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.labelDet.Location = new System.Drawing.Point(33, 424);
+            this.labelDet.Name = "labelDet";
+            this.labelDet.Size = new System.Drawing.Size(0, 31);
+            this.labelDet.TabIndex = 51;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1533, 833);
+            this.Controls.Add(this.labelDet);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label labelDet;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.RowCount = 1;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+
+                dataGridView1.RowCount = (int)numericUpDown1.Value;
+                dataGridView1.ColumnCount = (int)numericUpDown1.Value;
+                for (int i = 0; i < dataGridView1.ColumnCount; i++)
+                {
+                    dataGridView1.Columns[i].HeaderText = i.ToString();
+                }
+            }
+            else
+            {
+                dataGridView1.RowCount = (int)numericUpDown1.Value;
+                dataGridView1.ColumnCount = (int)numericUpDown1.Value;
+                Random rand = new Random();
+                for (int j = 0; j < dataGridView1.RowCount; j++)
+                {
+                    for (int i = 0; i < dataGridView1.ColumnCount; i++)
+                    {
+                        dataGridView1.Rows[j].Cells[i].Value = rand.Next(-100, 100);
+                        dataGridView1.Columns[i].HeaderText = i.ToString();
+                    }
+                }
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var myArray1 = new int[dataGridView1.RowCount, dataGridView1.ColumnCount];
+
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                {
+                    myArray1[i, j] = Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value);
+                }
+            }
+
+            double[][] m = MatrixCreate(dataGridView1.RowCount, dataGridView1.ColumnCount);
+            for(int i = 0; i < m.Length; i++)
+            {
+                for(int j = 0; j < m.Length; j++)
+                {
+                    m[i][j] = myArray1[i, j];
+                }
+            }
+
+            int[] perm;
+            int toggle;
+            double[][] luMatrix = MatrixDecompose(m, out perm, out toggle);
+            double[][] inverse = MatrixInverse(m);
+            double det = MatrixDeterminant(m);
+
+            for (int i = 0; i < inverse.Length; i++)
+            {
+                for (int j = 0; j < inverse.Length; j++)
+                {
+                    Console.WriteLine(inverse[i][j]);
+                }
+            }
+
+            labelDet.Text = "Определитель матрицы = " + det;
+
+            dataGridView3.RowCount = dataGridView1.RowCount;
+            dataGridView3.ColumnCount = dataGridView1.ColumnCount;
+
+            for (int i = 0; i < dataGridView3.RowCount; i++)
+            {
+                for (int j = 0; j < dataGridView3.ColumnCount; j++)
+                {
+                    dataGridView3.Rows[i].Cells[j].Value = inverse[i][j];
+                }
+            }
+        }
+
+        private double MatrixDeterminant(double[][] m)
+        {
+            int[] perm;
+            int toggle;
+            double[][] lum = MatrixDecompose(m, out perm, out toggle);
+            if (lum == null)
+                throw new Exception("Ошибка");
+            double result = toggle;
+            for (int i = 0; i < lum.Length; ++i)
+                result *= lum[i][i];
+            return result;
+        }
+
+        private double[][] MatrixInverse(double[][] m)
+        {
+            int n = m.Length;
+            double[][] result = MatrixDuplicate(m);
+
+            int[] perm;
+            int toggle;
+            double[][] lum = MatrixDecompose(m, out perm, out toggle);
+            if (lum == null)
+                throw new Exception("Ошибка");
+
+            double[] b = new double[n];
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = 0; j < n; ++j)
+                {
+                    if (i == perm[j])
+                        b[j] = 1.0;
+                    else
+                        b[j] = 0.0;
+                }
+
+                double[] x = HelperSolve(lum, b);
+
+                for (int j = 0; j < n; ++j)
+                    result[j][i] = x[j];
+            }
+            return result;
+        }
+
+        private double[] HelperSolve(double[][] luMatrix, double[] b)
+        {
+            int n = luMatrix.Length;
+            double[] x = new double[n];
+            b.CopyTo(x, 0);
+
+            for (int i = 1; i < n; ++i)
+            {
+                double sum = x[i];
+                for (int j = 0; j < i; ++j)
+                    sum -= luMatrix[i][j] * x[j];
+                x[i] = sum;
+            }
+
+            x[n - 1] /= luMatrix[n - 1][n - 1];
+            for (int i = n - 2; i >= 0; --i)
+            {
+                double sum = x[i];
+                for (int j = i + 1; j < n; ++j)
+                    sum -= luMatrix[i][j] * x[j];
+                x[i] = sum / luMatrix[i][i];
+            }
+
+            return x;
+        }
+
+        private double[][] MatrixDecompose(double[][] m, out int[] perm, out int toggle)
+        {
+            int rows = m.Length;
+            int cols = m[0].Length;
+            if (rows != cols)
+                throw new Exception("Attempt to MatrixDecompose a non-square mattrix");
+
+            int n = rows;
+
+            double[][] result = MatrixDuplicate(m);
+
+            perm = new int[n];
+            for (int i = 0; i < n; ++i) { perm[i] = i; }
+
+            toggle = 1;
+
+            for (int j = 0; j < n - 1; ++j)
+            {
+                double colMax = Math.Abs(result[j][j]);
+                int pRow = j;
+                for (int i = j + 1; i < n; ++i)
+                {
+                    if (result[i][j] > colMax)
+                    {
+                        colMax = result[i][j];
+                        pRow = i;
+                    }
+                }
+
+                if (pRow != j)
+                {
+                    double[] rowPtr = result[pRow];
+                    result[pRow] = result[j];
+                    result[j] = rowPtr;
+
+                    int tmp = perm[pRow];
+                    perm[pRow] = perm[j];
+                    perm[j] = tmp;
+
+                    toggle = -toggle;
+                }
+
+                if (Math.Abs(result[j][j]) < 1.0E-20)
+                    return null;
+
+                for (int i = j + 1; i < n; ++i)
+                {
+                    result[i][j] /= result[j][j];
+                    for (int k = j + 1; k < n; ++k)
+                    {
+                        result[i][k] -= result[i][j] * result[j][k];
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private double[][] MatrixDuplicate(double[][] m)
+        {
+            double[][] result = MatrixCreate(m.Length, m[0].Length);
+            for (int i = 0; i < m.Length; ++i)
+                for (int j = 0; j < m[i].Length; ++j)
+                    result[i][j] = m[i][j];
+            return result;
+        }
+
+        private double[][] MatrixCreate(int rowCount, int columnCount)
+        {
+            double[][] result = new double[rowCount][];
+            for (int i = 0; i < rowCount; ++i)
+                result[i] = new double[columnCount];
+
+            return result;
+        }
+    }
+}
